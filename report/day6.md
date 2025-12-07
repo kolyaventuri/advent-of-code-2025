bc043b072a88739b90ae253eb4e9b3577e3bc91f
Kolya Venturi <me@kolya.co>
2025-12-07 06:30:09 -0700

    (day6): Part2

--- report/day6.md
+++ report/day6.md
@@ -3,15 +3,15 @@ _This report automatically generated via `make report FILE=day6`_
 # Report for day6
 
 ## Performance over 5 runs, measured via node spawn + `performance.now()`
-- **Average**: 12.36ms
+- **Average**: 24.48ms
 
 ## Memory over 5 runs, measured by `time -l ./out/day6`
-- **Peak**: 1,144.54 KB
+- **Peak**: 1,157.41 KB
 
 ## Leak Report measured by `leaks --atExit --list -- ./out/day6`
 ```
 leaks Report Version: 3.0
-Process 54253: 169 nodes malloced for 16 KB
-Process 54253: 0 leaks for 0 total leaked bytes.
+Process 66588: 169 nodes malloced for 16 KB
+Process 66588: 0 leaks for 0 total leaked bytes.
 ```
 
--- src/day6.c
+++ src/day6.c
@@ -5,9 +5,10 @@ int main (void) {
   size_t size = 0;
   char *contents = read_file("input/day6.txt", &size);
   unsigned long long part1 = 0;
+  unsigned long long part2 = 0;
 
   int items = 0;
-  int *data = malloc(items * sizeof(int));
+  int *data = malloc(0);
   if (!data) return 1;
 
   int cols = 0;
@@ -16,12 +17,19 @@ int main (void) {
   // Some horrible parsing into one long int array
   int acc = 0;
   int index = 0; // Later tracking
+  int length = 0;
+  int temp_length = 0;
+  int acc_2 = 0;
+
   for (int i = 0; i < size; i++) {
     char c = contents[i];
+    temp_length++;
 
     // End of a number or line
     if (c == ' ' || c == '\n') {
       if (c == '\n') {
+        length = temp_length > length ? temp_length : length; 
+        temp_length = 0;
         rows++;
       }
 
@@ -39,7 +47,9 @@ int main (void) {
       } else {
         int count = items / rows;
         int operand = acc == -1 ? 0 : 1; // -1 = +, -2 = *
-        unsigned long temp = operand == 0 ? 0 : 1;
+        unsigned long temp_p1 = operand == 0 ? 0 : 1;
+        unsigned long temp_p2 = temp_p1;
+
         // printf("[");
         for (int j = 0; j < rows; j++) {
           int value = data[j * count + index];
@@ -52,18 +62,42 @@ int main (void) {
           // printf("%i", value);
           // if (j < rows -1) printf(", ");
           if (operand == 0) {
-            temp += value;
+            temp_p1 += value;
           } else {
-            temp *= value;
-          }
+            temp_p1 *= value;
+          } 
         } 
-        // printf("],");
-        part1 += temp;
-        // printf("\n");
+
+        // part2
+        int index2 = i - 1;
+        acc = 0;
+        while (acc == 0) {
+          for (int k = 0; k < rows; k++) {
+            int v = contents[index2 - (length * (rows - k))];
+            int parsed_v = v - '0';
+            if (parsed_v < 0) continue; 
+            acc_2 = (acc_2 * 10) + parsed_v; 
+          }
+          
+          if (operand == 0 && acc_2 > 0) {
+            temp_p2 += acc_2;
+          } else if (acc_2 > 0) {
+            temp_p2 *= acc_2;
+          }
+          
+          acc_2 = 0;
+          index2++;
+          int p = contents[index2];
+          acc = p == ' ' ? 0 : -1;
+        }
+
+        part1 += temp_p1;
+        part2 += temp_p2;
         index++;
       }
 
       acc = 0;
+      acc_2 = 0;
       continue;
     }
     
@@ -82,6 +116,7 @@ int main (void) {
   }
   
   printf("Part1: %llu\n", part1);
+  printf("Part2: %llu\n", part2);
 
   free(contents);
   free(data);
